32c0e821a9f92e228e50911a4ab7ab87d2c6b126
MonzurSiddique <134737939+MonzurSiddique@users.noreply.github.com>
2023-11-10 01:43:55 -0800

    Student database final assignment

---
+++ StudentDB/Migrations/202311100811435_CreateStudentListTable.cs
@@ -0,0 +1,26 @@
+﻿namespace StudentDB.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateStudentListTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.StudentLists",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.StudentLists");
+        }
+    }
+}
---
+++ StudentDB/Models/CsharpStudentDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace StudentDB.Models
+{
+    public class CsharpStudentDBContext : DbContext
+    {
+        public CsharpStudentDBContext() : base("CsharpConnection")
+        {
+
+        }
+        public DbSet<StudentList> StudentList { get; set; }
+    }
+}
