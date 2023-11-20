2a79f644649510886832d83d9e5922150bbdc5b0
MonzurSiddique <134737939+MonzurSiddique@users.noreply.github.com>
2023-11-20 12:55:31 -0800

    Method Class Assignment

---
+++ MethodClassAssignment1/Calculation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment1
+{
+    class Calculation
+    {
+        public int x;
+        public int y = 29;
+
+        public void AddTwoIntegers(int x, int y)
+        {
+ 
+            int result = x + x;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ MethodClassAssignment1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Calculation calculation = new Calculation();
+            calculation.AddTwoIntegers(14, 14);
+            Console.WriteLine(calculation.y);
+            Console.ReadLine();
+        }
+    }
+}
