5d08f145aa6d9d978a93b545029514e574a73cf3
MonzurSiddique <134737939+MonzurSiddique@users.noreply.github.com>
2023-12-03 10:14:41 -0800

    Car Insurance File submission

---
+++ CarInsuranceFileSubmission/ViewModels/InsureeVm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarInsurance.ViewModels
+{
+    public class InsureeVm
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal  Quote { get; set; }
+        public DateTime DateOfBirth { get; internal set; }
+        public int CarYear { get; internal set; }
+        public string CarMake { get; internal set; }
+        public string CarModel { get; internal set; }
+        public bool DUI { get; internal set; }
+        public int SpeedingTickets { get; internal set; }
+        public bool CoverageType { get; internal set; }
+    }
+}
